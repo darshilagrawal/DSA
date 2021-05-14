0ecca03991ad20bf1261b41b7c8be9929f1addab
darshilagrawal <darshilagrawal2000@gmail.com>
2021-05-14 23:20:45 +0530

    Finding the Point of transition in an Array

---
+++ TransitionPoint.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int transitionPoint(int arr[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cout << transitionPoint(a, n) << endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+int transitionPoint(int arr[], int n) {
+    // code here
+    int transition=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            transition=i;
+            break;
+        }else{
+            transition=-1;
+        }
+    }
+    return transition;
+}
