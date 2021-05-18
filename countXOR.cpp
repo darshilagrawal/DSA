8051431042fb39ef304bcb654aaa98df68fe8e1c
darshilagrawal <darshilagrawal2000@gmail.com>
2021-05-18 23:55:25 +0530

    Count the number of Pairs returning Zero as XOR

---
+++ countXOR.cpp
@@ -0,0 +1,49 @@
+// C++ program to find number 
+// of pairs in an array such
+// that their XOR is 0
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to calculate the
+// count
+long long int calculate(int a[], int n);
+
+// Driver Code
+int main()
+{
+
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	  int n;
+	  cin>>n;
+	  int arr[n+1];
+	  for( int i=0;i<n;i++)
+	        cin>>arr[i];
+	    cout << calculate(arr, n)<<endl;
+        
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+long long int calculate(int a[], int n)
+{
+    sort(a,a+n);
+    int count=1;
+    int answer=0;
+    for(int i=1;i<n;i++){
+        if(a[i]==a[i-1]){
+            count++;
+        }else{
+            answer=answer+(count*(count-1))/2;
+            count=1;
+        }
+    }
+    answer=answer+(count*(count-1))/2;
+    // Complete the function
+    return answer;
+}
